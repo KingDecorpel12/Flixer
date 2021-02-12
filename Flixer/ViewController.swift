27971c10bdfc42d7b65f17d63c8b35dfb875e827
Omar Hegazy <omarhegazy78@gmail.com>
2021-02-12 12:28:48 -0500

    Initial Commit

---
+++ Flixer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixer
+//
+//  Created by Omar Hegazy on 2/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
